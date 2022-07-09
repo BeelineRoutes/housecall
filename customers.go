f6d286b16d12590743bf6b36116fac742f59cc30
NateDogg <nathan@necustomsoftware.com>
2022-07-09 15:18:59 -0400

    pages through customer responses and returns all that we get from a search

--- customers.go
+++ customers.go
@@ -23,25 +23,36 @@ import (
 //-----------------------------------------------------------------------------------------------------------------------//
 
 // Returns a list of the customers using a 'simple' searching keyword or short address part
-// returns in order of most recently created. limiting this to 100 matches, if they don't search well, no reason to return all of them
+// returns in order of most recently created.
+// converted to page through the results, allows us to do an empty search for customers
 func (this *HouseCall) SearchCustomers (ctx context.Context, token, search string) ([]Customer, error) {
     header := make(map[string]string)
     header["Authorization"] = "Bearer " + token 
 
     params := url.Values{}
-    params.Set("page_size", "100")
+    params.Set("page_size", "200")
     params.Set("sort_direction", "desc")
     params.Set("sort_by", "created_at")
     params.Set("q", search)
 
-    resp := customerListResponse{}
-    
-    errObj, err := this.send (ctx, http.MethodGet, fmt.Sprintf("customers?%s", params.Encode()), header, nil, &resp)
-    if err != nil { return nil, errors.WithStack(err) } // bail
-    if errObj != nil { return nil, errObj.Err() } // something else bad
+    var ret []Customer
 
-    // we're here, we're good
-    return resp.Customers, nil 
+    for i := 1; i <= 1000; i++ { // stay in a loop as long as we're pulling jobs
+        params.Set("page", fmt.Sprintf("%d", i)) // set our next page
+
+        resp := customerListResponse{}
+    
+        errObj, err := this.send (ctx, http.MethodGet, fmt.Sprintf("customers?%s", params.Encode()), header, nil, &resp)
+        if err != nil { return nil, errors.WithStack(err) } // bail
+        if errObj != nil { return nil, errObj.Err() } // something else bad
+
+        // we're here, we're good
+        ret = append (ret, resp.Customers...)
+        
+        if i >= resp.TotalPages { return ret, nil } // we finished
+    }
+    // this only happens if we have too many pages... shouldn't happen anyway, but return what we got
+    return ret, nil 
 }
 
 // creates the customer and returns their id
